3ca1addf845c84d78bdf1eedb90b6432b23bd647
GabrielEdid <gabrieledid28@gmail.com>
2023-10-11 10:39:10 -0600

    Changes

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <p>Hello From React! Hagamos de esto algo increible, te late Dani?
+        <p>Hello From React! Que se venga lo bueno, te late Dani?
         </p>
         <a
           className="App-link"
