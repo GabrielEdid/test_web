b4fcd68495c711c6df9f4ffb005afa06e57c0e08
GabrielEdid <gabrieledid28@gmail.com>
2023-10-17 18:48:03 -0600

    Se añadio APIMeLi y modificaciones en PopUp

---
+++ src/APIs/APIMeLi.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+/*export default axios.create({
+    URLbase: "",
+    headers: {
+      Authorization:
+    },
+  });
+  */
--- src/Components/PopUp/PopUp.js
+++ src/Components/PopUp/PopUp.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 class PopUp extends React.Component {
     openWindow = () => {
         const url = 'https://auth.mercadolibre.com.mx/authorization?response_type=code&client_id=3516733548819218&redirect_uri=https://test-web-gabriel-95557e5aa8fc.herokuapp.com/';
-        window.open(url, '_blank', 'noopener,noreferrer');
+        window.open(url, '_self', 'noopener,noreferrer');
+    }
+
+    componentDidMount() {
+        const urlParams = new URLSearchParams(window.location.search);
+        const code = urlParams.get('code');
+        if (code) {
+            console.log("User has returned with code:", code);
+            console.log("Full redirected URL:", window.location.href);
+            // You can handle the code here, e.g., send it to your server for further processing
+        }
     }
 
     render() {
