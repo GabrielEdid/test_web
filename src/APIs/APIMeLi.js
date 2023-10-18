80413b303f05423a737858a9a09f8fd0c6c9dbfb
GabrielEdid <gabrieledid28@gmail.com>
2023-10-17 22:59:23 -0600

    Se añade mensaje de Code

--- src/APIs/APIMeLi.js
+++ src/APIs/APIMeLi.js
@@ -1,9 +1,36 @@
+import React from 'react';
 import axios from 'axios';
+import PopUp from './PopUp';  // Ensure this path is correct
 
-/*export default axios.create({
-    URLbase: "",
-    headers: {
-      Authorization:
-    },
-  });
-  */
+class APIMeLi extends React.Component {
+    fetchAccessToken = (code) => {
+        // Define the API endpoint and your data
+        const endpoint = 'https://api.mercadolibre.com/oauth/token';
+        const data = {
+            client_id: '3516733548819218',
+            grant_type: 'authorization_code',
+            client_secret: 'w7sU8woCroDxWxMeqXx5TOI1ETPKHeTr',
+            redirect_uri: 'https://test-web-gabriel-95557e5aa8fc.herokuapp.com/',
+            code: code  // Use the code here
+        };
+
+        // Make the POST request
+        axios.post(endpoint, data)
+        .then(response => {
+            // Extract the access token from the response
+            const accessToken = response.data.access_token;
+            console.log('Access Token:', accessToken);
+            // You can store the token in local storage or use it as needed
+            // localStorage.setItem('access_token', accessToken);
+        })
+        .catch(error => {
+            console.log('Error fetching the access token:', error);
+        });
+    }
+
+    render() {
+        return <PopUp sendCodeToFetchToken={this.fetchAccessToken} />
+    }
+}
+
+export default APIMeLi;
--- src/Components/PopUp/PopUp.js
+++ src/Components/PopUp/PopUp.js
@@ -1,6 +1,10 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 
 class PopUp extends React.Component {
+    state = {
+        code: null
+    };
+
     openWindow = () => {
         const url = 'https://auth.mercadolibre.com.mx/authorization?response_type=code&client_id=3516733548819218&redirect_uri=https://test-web-gabriel-95557e5aa8fc.herokuapp.com/';
         window.open(url, '_self', 'noopener,noreferrer');
@@ -12,15 +16,28 @@ class PopUp extends React.Component {
         if (code) {
             console.log("User has returned with code:", code);
             console.log("Full redirected URL:", window.location.href);
-            // You can handle the code here, e.g., send it to your server for further processing
+            
+            // Now, send this code to the function in fetchToken file
+            this.props.sendCodeToFetchToken(code);
+            
+            // Update the state with the code
+            this.setState({ code });
+        }
+        else {
+            console.log("Oops! Something Happened!")
         }
     }
 
     render() {
         return (
-            <button onClick={this.openWindow}>
-                Iniciar Sesion con Mercado Libre
-            </button>
+            <>
+                <button onClick={this.openWindow}>
+                    Iniciar Sesion con Mercado Libre
+                </button>
+                <h1>
+                    El Code es: {this.state.code}
+                </h1>
+            </>
         );
     }
 }
