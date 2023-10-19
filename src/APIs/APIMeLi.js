6f951a7989485053b4cb32cb3de2303362f186fa
GabrielEdid <gabrieledid28@gmail.com>
2023-10-18 19:04:36 -0600

    Se cambio APIMeLi

--- src/APIs/APIMeLi.js
+++ src/APIs/APIMeLi.js
@@ -1,36 +1,26 @@
-import React from 'react';
 import axios from 'axios';
-import PopUp from './PopUp';  // Ensure this path is correct
 
-class APIMeLi extends React.Component {
-    fetchAccessToken = (code) => {
-        // Define the API endpoint and your data
-        const endpoint = 'https://api.mercadolibre.com/oauth/token';
-        const data = {
-            client_id: '3516733548819218',
-            grant_type: 'authorization_code',
-            client_secret: 'w7sU8woCroDxWxMeqXx5TOI1ETPKHeTr',
-            redirect_uri: 'https://test-web-gabriel-95557e5aa8fc.herokuapp.com/',
-            code: code  // Use the code here
-        };
+// API Function to fetch the access token
+export const fetchAccessToken = async (code) => {
+    // Define the API endpoint and your data
+    const endpoint = 'https://api.mercadolibre.com/oauth/token';
+    const data = {
+        client_id: '3516733548819218',
+        grant_type: 'authorization_code',
+        client_secret: 'w7sU8woCroDxWxMeqXx5TOI1ETPKHeTr',
+        redirect_uri: 'https://test-web-gabriel-95557e5aa8fc.herokuapp.com/',
+        code: code
+    };
 
-        // Make the POST request
-        axios.post(endpoint, data)
-        .then(response => {
-            // Extract the access token from the response
-            const accessToken = response.data.access_token;
-            console.log('Access Token:', accessToken);
-            // You can store the token in local storage or use it as needed
-            // localStorage.setItem('access_token', accessToken);
-        })
-        .catch(error => {
-            console.log('Error fetching the access token:', error);
-        });
+    try {
+        const response = await axios.post(endpoint, data);
+        const accessToken = response.data.access_token;
+        console.log('Access Token:', accessToken);
+        // You can store the token in local storage or use it as needed
+        // localStorage.setItem('access_token', accessToken);
+        return accessToken;  // Return the token so the calling function can use it
+    } catch (error) {
+        console.log('Error fetching the access token:', error);
+        throw error;  // Throw the error to be handled by the calling function
     }
-
-    render() {
-        return <PopUp sendCodeToFetchToken={this.fetchAccessToken} />
-    }
-}
-
-export default APIMeLi;
+};
